2fc543d6aefa4967ebf184edca828ea8e0ae8cdb
Scott Cytacki <scytacki@concord.org>
2015-08-17 22:56:14 -0400

    basic add keys to all the projects
    This doesn’t really handle the errors well yet.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ ruby '2.2.1'
 source 'https://rubygems.org'
 
 gem 'sinatra', '~> 1.4.6'
+gem 'rest-client', '~> 1.8.0'
+
+group :development do
+  gem 'rerun'
+end
--- make-keys.rb
+++ make-keys.rb
@@ -1,5 +1,38 @@
 require 'sinatra'
+require 'json'
+require 'rest-client'
+
+project_list_response =
+  RestClient.get ENV['ISENSE_PROJECTS_URL']
+
+project_list = JSON.parse project_list_response.to_str
 
 get '/' do
-  "Hello There"
+  body project_list.to_json
+end
+
+post '/' do
+  request.body.rewind
+  request_payload = JSON.parse request.body.read
+
+  # RestClient.post "http://example.com/resource", { 'x' => 1 }.to_json, :content_type => :json, :accept => :json
+
+  add_key_data = {
+  	email: ENV['ISENSE_EMAIL'],
+  	password: ENV['ISENSE_PASSWORD'],
+  	contrib_key: request_payload
+  }
+
+  project_list.each{|project|
+  	begin
+	  add_key_response =
+        RestClient.post "https://isenseproject.org/api/v1/projects/#{project['id']}/add_key",
+          add_key_data.to_json, :content_type => :json, :accept => :json
+      puts add_key_response
+    rescue RestClient::Exception => e
+      puts e.response
+    end
+  }
+
+  body request_payload.to_json
 end
